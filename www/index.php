17bd3560f2157700026198b3fa8964b84a819188
Jaroslav Hranička <hranicka@outlook.com>
2016-02-14 10:31:11 +0100

    index.php: PhpStorm & Symfony Console workaround

--- www/index.php
+++ www/index.php
@@ -5,4 +5,10 @@
 
 $container = require __DIR__ . '/../app/bootstrap.php';
 
+// PhpStorm & Symfony Console
+// See https://youtrack.jetbrains.com/issue/WI-29627
+if (isset($argv[1]) && $argv[1] === '-V') {
+	die('Symfony version 2.8.0');
+}
+
 $container->getByType('Nette\Application\Application')->run();
