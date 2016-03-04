819c65d6e025498ee0211c16ddd3fd1700a907f3
Jaroslav Hranička <hranicka@outlook.com>
2016-03-04 17:32:56 +0100

    Maintenance & Deploy jobs support added

--- www/index.php
+++ www/index.php
@@ -1,8 +1,32 @@
 <?php
 
-// Uncomment this line if you must temporarily take down your site for maintenance.
-// require __DIR__ . '/.maintenance.php';
+namespace Etten\App;
 
-/** @var \Etten\App\App $app */
 $app = require __DIR__ . '/../app/bootstrap.php';
+
+$maintainer = new Maintainer();
+$locker = new Locker();
+
+// Add some IPs for deploy permission.
+$maintainer->developers[] = '192.168.1.1';
+
+// Lock the Application
+if ($maintainer->isJob('disable')) {
+	$locker->lock();
+	exit;
+}
+
+// Clean caches and unlock the Application.
+if ($maintainer->isJob('enable')) {
+	(new Cleaner($app))->clean();
+	$locker->unlock();
+	exit;
+}
+
+// If locked, show a Maintenance site.
+if ($locker->isLocked()) {
+	require __DIR__ . '/.maintenance.php';
+}
+
+// Finally run the Application.
 $app->run();
