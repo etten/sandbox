723204a12f87faaf0b7058755eb7a7e250963250
Jaroslav Hranička <hranicka@outlook.com>
2016-03-07 12:00:05 +0100

    Maintenance: Run Migrations

--- www/index.php
+++ www/index.php
@@ -5,25 +5,33 @@ namespace Etten\App;
 /** @var App $app */
 $app = require __DIR__ . '/../app/bootstrap.php';
 
+$maintainer = $app->createMaintainer();
+
 $locker = new Locker();
 
 // Lock the Application
-if ($app->isMaintainerJob('disable')) {
+$maintainer->addJob('disable', function () use ($locker) {
 	$locker->lock();
 	exit;
-}
+});
 
-// Clean caches and unlock the Application.
-if ($app->isMaintainerJob('enable')) {
+// Clean caches, then run Migrations.
+$maintainer->addJob('enable', function () use ($app) {
 	(new Cleaner($app))->clean();
+	(new Console($app))->run('migrations:continue');
+});
+
+// Unlock the Application - it's ready.
+$maintainer->addJob('enable', function () use ($locker) {
 	$locker->unlock();
 	exit;
-}
+});
 
-// If locked, show a Maintenance site.
+$maintainer->runJobs();
+
+// If locked, show a Maintenance site, otherwise run the App.
 if ($locker->isLocked()) {
 	require __DIR__ . '/.maintenance.php';
+} else {
+	$app->run();
 }
-
-// Finally run the Application.
-$app->run();
