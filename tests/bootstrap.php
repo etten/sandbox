6d0155b9334639eee6037c83657cb73fb80eebb0
Jaroslav Hranička <hranicka@outlook.com>
2016-03-21 13:01:24 +0100

    tests/bootstrap.php refactoring

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,10 +7,10 @@ use Etten;
 /** @var Etten\App\App $app */
 $app = require __DIR__ . '/../app/bootstrap.php';
 
-Etten\App\Tests\ContainerTestCase::$app = $app;
-
 $app->addBootstrapFile(__DIR__ . '/bootstrap.neon');
 $app->addConfigFile(__DIR__ . '/config.local.neon', 'local');
 $app->createConfigurator();
+
+Etten\App\Tests\ContainerTestCase::$app = $app;
 
 return $app;
