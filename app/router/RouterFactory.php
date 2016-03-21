c2140873278efbb01120b09d5b69889c6643d86d
Jaroslav Hranička <hranicka@outlook.com>
2016-03-21 11:24:44 +0100

    RouteFactory::createRouter -> ::create (and is not static)

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -11,7 +11,7 @@ class RouterFactory
 	/**
 	 * @return Nette\Application\IRouter
 	 */
-	public static function createRouter()
+	public function create()
 	{
 		$router = new Routers\RouteList();
 		$router[] = new Routers\Route('<presenter>/<action>[/<id>]', 'Homepage:default');
