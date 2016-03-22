ccb625a8f182ee7324e94a295d0d8e5f460dfeda
Jaroslav Hranička <hranicka@outlook.com>
2016-03-22 09:55:48 +0100

    Tests namespaces improvements

--- tests/control/HomepagePresenterTest.php
+++ tests/control/HomepagePresenterTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests;
+namespace Tests\Control;
 
 use App;
 use Etten;
--- tests/integration/Articles/ArticlePersistingTest.php
+++ tests/integration/Articles/ArticlePersistingTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Articles;
+namespace Tests\Integration\Articles;
 
 use App\Models\Articles;
 use App\Models\Routes;
