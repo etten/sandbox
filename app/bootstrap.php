8dca5bd80cd9b976b12b07d8b03a4e24b99631ad
Jaroslav Hranička <hranicka@outlook.com>
2016-04-20 08:42:27 +0200

    Require etten/app v2.3 (better CLI support)

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -13,6 +13,7 @@ $app->addBootstrapFile(__DIR__ . '/config/bootstrap.neon');
 $app->addConfigFile(__DIR__ . '/config/config.neon');
 $app->addConfigFile(__DIR__ . '/config/config.local.neon', 'local');
 
+$app->addExtension(new Etten\App\Extensions\KdybyConsole());
 $app->addExtension(new Etten\App\Extensions\SystemSetup());
 $app->addExtension(new Etten\App\Extensions\PhpStormSymfonyConsoleFix());
 
